52816133f3bbc4e0e9af1238185516a60b987af4
DiegoCantillo <diegocantymez@gmail.com>
2023-07-06 09:49:18 -0500

    update

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,12 +1,15 @@
-
+import SectionHomeFour from "./SectionHomeFour";
 import SectionHomeOne from "./SectionHomeOne";
 import SectionHomeTwo from "./SectionHomeTwo";
 
 function Home() {
-  return [
-    <SectionHomeOne key="sectionHomeOne" />,
-    <SectionHomeTwo key="sectionHomeTwo" />,
-  ];
+  return (
+    <>
+      <SectionHomeOne key="sectionHomeOne" />,
+      <SectionHomeTwo key="sectionHomeTwo" />,
+      <SectionHomeFour />
+    </>
+  );
 }
 
 export default Home;
---
+++ frontend/src/components/SectionHomeFour.tsx
@@ -0,0 +1,13 @@
+
+const SectionHomeFour = () => {
+    return (
+        <div className="h-screen bg-slate-400">
+            <div>
+                <h2>Busqueda Recientes</h2>
+            </div>
+        </div>
+    );
+};
+
+export default SectionHomeFour
+
