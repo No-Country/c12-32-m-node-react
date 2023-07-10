692ff5810f9c309a5a6ea0c0a80c0ec5d8b5a800
Adrian <esteban.adrian.gonz@gmail.com>
2023-07-10 10:51:48 -0300

    Added modules

--- backend/src/modules.ts
+++ backend/src/modules.ts
@@ -1,3 +1,5 @@
 import { AuthModule } from './modules/auth/auth.module';
+import { PetsModule } from './modules/pets/pets.module';
+import { PostsModule } from './modules/posts/posts.module';
 
-export const appModules = [AuthModule];
+export const appModules = [AuthModule, PetsModule, PostsModule];
