fa867fbaeca2db64a7d05f93e6f114931ea822ea
Adrian <esteban.adrian.gonz@gmail.com>
2023-07-10 10:41:00 -0300

    Auth Entity defined

--- backend/src/modules/auth/entities/auth.entity.ts
+++ backend/src/modules/auth/entities/auth.entity.ts
@@ -1,7 +1,35 @@
 import { BasedEntity } from 'src/modules/shared/entities/base.entity';
 import { Entity, Column } from 'typeorm';
 
-@Entity({name: 'users'})
+enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+@Entity({ name: 'users' })
 export class UserEntity extends BasedEntity {
-  
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: true })
+  last_name: string;
+
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: true })
+  phone: number;
+
+  @Column({ nullable: true })
+  city: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER,
+    nullable: false,
+  })
+  role: string;
 }
