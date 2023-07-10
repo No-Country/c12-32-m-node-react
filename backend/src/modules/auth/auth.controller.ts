e45e24835b1ab13197f4f8bbffd41b006f541441
Adrian <esteban.adrian.gonz@gmail.com>
2023-07-10 10:56:08 -0300

    Formatted

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
@@ -19,16 +27,16 @@ export class AuthController {
 
   @Get(':id')
   findOneBy(@Param('id') id: string) {
-    return this.authService.findOneBy({id});
+    return this.authService.findOneBy({ id });
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update({id}, updateAuthDto);
+    return this.authService.update({ id }, updateAuthDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.authService.delete({id});
+    return this.authService.delete({ id });
   }
 }
