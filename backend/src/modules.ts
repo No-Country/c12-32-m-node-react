a40033a06547735b09067053d141cd45ec133fce
ivan soruco <ivan.soruco@gmail.com>
2023-07-19 21:46:23 -0400

    Revert "nada que cambiar"
    This reverts commit 44c066e884cd0fd997c7e6e48fc98eb6066a596b.

--- backend/src/modules.ts
+++ backend/src/modules.ts
@@ -3,4 +3,4 @@ import { PetsModule } from './modules/pets/pets.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { SharedModule } from './modules/shared/shared.module';
 
-export const appModules = [AuthModule,PetsModule,PostsModule, SharedModule];
+export const appModules = [AuthModule, PetsModule, PostsModule, SharedModule];
--- backend/src/modules/pets/dto/create-pets.dto.ts
+++ backend/src/modules/pets/dto/create-pets.dto.ts
@@ -1,28 +1,37 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePetsDto {
-   
-    @IsString()
-    name: string;
+  @IsNotEmpty()
+  @ApiProperty({ description: 'name' })
+  @IsString()
+  name: string;
 
-    @IsString()
-    description: string;
-     
-    @IsNumber()
-    size: number;
+  @IsString()
+  @ApiProperty({ description: 'description' })
+  description: string;
 
-    @IsNumber()
-    age: number;
+  @IsNumber()
+  @ApiProperty({ description: 'size' })
+  size: number;
 
-    @IsString()
-    gender: string;
-  
-    @IsString()
-    lastSeen: string;
-  
-    @IsString()
-    race: string;
+  @IsNumber()
+  @ApiProperty({ description: 'age' })
+  age: number;
 
-    @IsString()
-    user_id:string
+  @IsString()
+  @ApiProperty({ description: 'gender' })
+  gender: string;
+
+  @IsString()
+  @ApiProperty({ description: 'lastSeen' })
+  lastSeen: string;
+
+  @IsString()
+  @ApiProperty({ description: 'race' })
+  race: string;
+
+  @IsString()
+  @ApiProperty({ description: 'token' })
+  token: string;
 }
