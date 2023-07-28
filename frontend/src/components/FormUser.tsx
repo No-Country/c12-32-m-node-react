d4b0cf3a812b787b7841def040552e79c7f8c783
Magali Ruiz Diaz <114106304+maggieruizdiaz22@users.noreply.github.com>
2023-07-28 18:27:13 -0400

    Update FormUser.tsx

--- frontend/src/components/FormUser.tsx
+++ frontend/src/components/FormUser.tsx
@@ -1,11 +1,8 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
-import {
-  SET_ACTIVE_USER,
-  selectEmail,
-} from "./redux/slice/authSlice";
+import { SET_ACTIVE_USER, selectEmail } from "./redux/slice/authSlice";
 import swal from "sweetalert";
 import "sweetalert2/dist/sweetalert2.min.css";
 import axios from "axios";
@@ -16,16 +13,16 @@ const FormUser = () => {
     handleSubmit,
     register,
     formState: { errors },
-  } = useForm();
-  
+  } = useForm({});
+
   const dispatch = useDispatch();
   const userEmail = useSelector(selectEmail);
   const userEmailSlice = userEmail?.split("@")[0];
   const [displayName] = useState("");
 
   const [imagenes, setImagenes] = useState<string[]>([]);
+  const [userImg, setUserImg] = useState<string | null>(null);
 
-  
   useEffect(() => {
     dispatch(
       SET_ACTIVE_USER({
@@ -36,106 +33,91 @@ const FormUser = () => {
     );
   }, [dispatch]);
 
+  const formData = new FormData();
   // Codigo para la funcion de tomar una imagen de tu pc
-const handleAgregarImagen = (event: React.ChangeEvent<HTMLInputElement>) => {
-  const files = event.target.files;
-  if (files) {
-    const newImagenes: string[] = [];
-    Array.from(files).forEach((file) => {
-      const reader = new FileReader();
-      reader.onload = () => {
-        if (reader.result) {
-          newImagenes.push(reader.result as string);
-          // Utiliza la función setState con una función callback para acceder al estado actualizado
-          setImagenes((prevImagenes) => [
-            ...prevImagenes,
-            reader.result as string,
-          ]);
-        }
-      };
-      reader.readAsDataURL(file);
-    });
-
-    console.log([...imagenes, ...newImagenes]); // Aquí aún puede mostrar el estado desactualizado
-  }
+  const handleAgregarImagen = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.files);
+    const fileNuevo = event.target.files;
+    for (let i = 0; i < fileNuevo!.length; ++i) {
+      formData.append("file", fileNuevo![i]);
+    }
+formData.forEach((value, key) => {
+  console.log(key, value);
+});
   };
-  
-    const handleRemoveImagen = (index: number) => {
-      // uso el index para borrar la imagen del estado
-      const updatedImagenes = [...imagenes];
-      updatedImagenes.splice(index, 1);
-      setImagenes(updatedImagenes);
-    };
 
-const getTokenFromLocalStorage = (): string | null => {
-  const token = localStorage.getItem("token");
-  console.log("Token almacenado en localStorage:", token);
-  return token;
-};
-const submit = async (data: any) => {
-  try {
-    // Si no envían ninguna imagen, no deja subir el formulario
-    if (!imagenes || imagenes.length === 0) {
-      swal("Warning", "Debes seleccionar por lo menos una imagen", "error");
-    }
+  const handleRemoveImagen = (index: number) => {
+    // uso el index para borrar la imagen del estado
+    const updatedImagenes = [...imagenes];
+    updatedImagenes.splice(index, 1);
+    setImagenes(updatedImagenes);
+  };
 
-    // Obtener el token de autenticación desde el localStorage
-   const token = getTokenFromLocalStorage();
+  const getTokenFromLocalStorage = (): string | null => {
+    const token = localStorage.getItem("token");
+    console.log("Token almacenado en localStorage:", token);
+    return token;
+  };
 
-    if (!token) {
-      swal("Error", "Token no encontrado", "error");
-      return;
-    }
+  // el objeto de datos que se enviará al backend
 
-    // Agregar el token al encabezado de autorización
-    const config = {
-      headers: {
-        Authorization: `${token}`,
-      },
-    };
+  const submit = async (data: any) => {
+    try {
+      // Si no envían ninguna imagen, no deja subir el formulario
+      // if (!imagenes || imagenes.length === 0) {
+      //   swal("Warning", "Debes seleccionar por lo menos una imagen", "error");
+      // }
+
+      // Obtener el token de autenticación desde el localStorage
+      const token = getTokenFromLocalStorage();
+
+      if (!token) {
+        swal("Error", "Token no encontrado", "error");
+        return;
+      }
+
+      // Agregar el token al encabezado de autorización
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
       console.log("Encabezado de autorización:", config.headers.Authorization);
 
-    // el objeto de datos que se enviará al backend 
-    const formData = {
-      name: data.name,
-      description: data.description,
-      size: Number(data.size),
-      age: Number(data.age),
-      gender: data.gender,
-      lastSeen: data.lastSeen,
-      category: data.category,
-      phone: data.phone,
-      direction: data.direction,
-      descriptionPublication: data.descriptionPublication,
-    };
+      // Crear un objeto formData
 
-    // Solicitud al backend para crear una nueva mascota
-    const response = await axios.post(
-      "http://localhost:7500/api/v1/api/v1/pets",
-      formData,
-      config
-    );
+      formData.append("pet_name", data.pet_name);
+      formData.append("title", data.title);
+      formData.append("pet_description", data.pet_description);
+      formData.append("size", data.size);
+      formData.append("age", data.age);
+      formData.append("gender", data.gender);
+      formData.append("lastSeen", data.lastSeen);
+      formData.append("post_category", data.post_category);
+      formData.append("phone", data.phone);
+      formData.append("direction", data.direction);
 
-    // Tomar el ID de la mascota creada
-    const petId = response.data.data;
-    console.log("ID de la mascota creada:", petId);
-
-    // Subir las imágenes asociadas a la mascota
-    const uploadImagesResponse = await axios.post(
-      `http://localhost:7500/api/v1/pets-images/${petId}/`,
-      formData,
-      config
-    );
-
-    console.log("imágenes:", uploadImagesResponse.data);
-
-    swal("Éxito", "Anuncio publicado correctamente", "success");
-    navigate("/profile");
-  } catch (error) {
-    console.error("Error al enviar el formulario:", error);
-    swal("Error", "Ocurrió un error al enviar el formulario", "error");
-  }
+      // Solicitud al backend para crear una nueva mascota
+      const response = await axios.post(
+        "https://petsociety.up.railway.app/api/v1/pets",
+        formData,
+        config
+      );
+      console.log(response);
+      swal("Éxito", "Anuncio publicado correctamente", "success");
+      navigate("/profile");
+    } catch (error) {
+      console.error("Error al enviar el formulario:", error);
+      swal("Error", "Ocurrió un error al enviar el formulario", "error");
+    }
   };
+
+   useEffect(() => {
+     const storedImage = localStorage.getItem("userProfileImage");
+     if (storedImage) {
+       setUserImg(storedImage);
+     }
+   }, []);
 
   return (
     <div className="lg:flex flex-initial">
@@ -144,19 +126,82 @@ const submit = async (data: any) => {
           <h1 className="text-gray-600 text-2xl tracking-widest mb-8">
             AGREGAR IMÁGENES
           </h1>
+
+          <NavLink to={"/"}>
+            <button className="absolute top-20 left-0 m-4 bg-customBgNavBar hover:bg-gray-500 text-white font-medium py-2 px-4 rounded-md">
+              Volver
+            </button>
+          </NavLink>
+        </div>
+        <div>
+          <div className="grid grid-cols-3 gap-4 mt-[-15rem]">
+            {imagenes.map((imagen, index) => (
+              <div key={index} className="relative">
+                <img
+                  src={imagen}
+                  alt={`Imagen ${index + 1}`}
+                  className="mb-4 w-[15rem] h-auto"
+                />
+                <button
+                  className="absolute top-0 right-0 bg-red-500 text-white font-medium py-1 px-2 rounded-md"
+                  onClick={() => handleRemoveImagen(index)}
+                >
+                  X
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 p-4">
+        <form encType="multipart/form-data" onSubmit={handleSubmit(submit)}>
+          <div className="flex items-center ml-[-2rem] mb-4 mt-4">
+            <label htmlFor="input-foto" className="mr-2 lg:pl-10"></label>
+              <img
+                className="w-12 h-12 sm:w-24 sm:h-24 bg-gray-300 transition duration-300 hover:bg-gray-600 rounded-full"
+                src={userImg?.length ? userImg : ""}
+                alt="Foto de perfil"
+              />
+            <p className="lg:pl-5 pl-4 text-lg font-semibold tracking-widest">
+              {displayName ? <>{displayName}</> : <>{userEmailSlice}</>}
+            </p>
+            <div className="ml-4 lg:ml-60 mb-4">
+              <label htmlFor="category" className="block mb-2 font-semibold">
+                Categoría:
+              </label>
+              <select
+                id="post_category"
+                {...register("post_category", {
+                  required: "Seleccionar categoría es obligatorio",
+                })}
+                className="w-full p-2 border border-gray-300 rounded-md"
+              >
+                <option value="">Seleccionar categoría</option>
+                <option value="categoria1">Adopción</option>
+                <option value="categoria2">Mascota Perdida</option>
+              </select>
+              {errors.post_category &&
+                typeof errors.post_category.message === "string" && (
+                  <p className="text-red-500">{errors.post_category.message}</p>
+                )}
+            </div>
+          </div>
+          <h2 className="text-xl font-semibold mb-4 mt-14 lg:ml-8">
+            Datos de la mascota
+          </h2>
           <div className="mb-4 flex items-center">
             <input
               type="file"
               multiple
-              id="input-imagenes"
+              id="file"
               accept="image/*"
               className="hidden"
-              {...register("images")}
+              {...register("file")}
               onChange={handleAgregarImagen}
             />
             <label
-              htmlFor="input-imagenes"
-              className="p-4 border border-dashed cursor-pointer rounded-lg"
+              htmlFor="file"
+              className="p-4  border border-dashed cursor-pointer rounded-lg"
             >
               <svg
                 className="w-6 h-6 text-gray-500"
@@ -171,68 +216,10 @@ const submit = async (data: any) => {
               </svg>
             </label>
           </div>
-          <NavLink to={"/"}>
-            <button className="absolute top-20 left-0 m-4 bg-customBgNavBar hover:bg-gray-500 text-white font-medium py-2 px-4 rounded-md">
-              Volver
-            </button>
-          </NavLink>
-        </div>
-        <div>
-         <div className="grid grid-cols-3 gap-4 mt-[-15rem]">
-  {imagenes.map((imagen, index) => (
-    <div key={index} className="relative">
-      <img
-        src={imagen}
-        alt={`Imagen ${index + 1}`}
-        className="mb-4 w-[15rem] h-auto"
-      />
-      <button
-        className="absolute top-0 right-0 bg-red-500 text-white font-medium py-1 px-2 rounded-md"
-        onClick={() => handleRemoveImagen(index)}
-      >
-        X
-      </button>
-    </div>
-  ))}
-</div>
-        </div>
-      </div>
-      <div className="w-full md:w-1/2 p-4">
-        <form onSubmit={handleSubmit(submit)}>
-          <div className="flex items-center ml-[-2rem] mb-4 mt-4">
-            <label htmlFor="input-foto" className="mr-2 lg:pl-10"></label>
-            <div className="w-12 h-12 sm:w-24 sm:h-24 bg-gray-300 transition duration-300 hover:bg-gray-600 rounded-full"></div>
-            <p className="lg:pl-5 pl-4 text-lg font-semibold tracking-widest">
-              {displayName ? <>{displayName}</> : <>{userEmailSlice}</>}
-            </p>
-            <div className="ml-4 lg:ml-60 mb-4">
-              <label htmlFor="category" className="block mb-2 font-semibold">
-                Categoría:
-              </label>
-              <select
-                id="category"
-                {...register("category", {
-                  required: "Seleccionar categoría es obligatorio",
-                })}
-                className="w-full p-2 border border-gray-300 rounded-md"
-              >
-                <option value="">Seleccionar categoría</option>
-                <option value="categoria1">Adopción</option>
-                <option value="categoria2">Mascota Perdida</option>
-              </select>
-              {errors.category &&
-                typeof errors.category.message === "string" && (
-                  <p className="text-red-500">{errors.category.message}</p>
-                )}
-            </div>
-          </div>
-          <h2 className="text-xl font-semibold mb-4 mt-14 lg:ml-8">
-            Datos de la mascota
-          </h2>
           <div className="lg:flex mb-4">
             <div className="lg:mr-16 lg:mb-10 lg:mt-10 lg:ml-2">
               <label
-                htmlFor="name"
+                htmlFor="pet_name"
                 style={{ display: "block" }}
                 className="mr-2 font-semibold"
               >
@@ -240,13 +227,14 @@ const submit = async (data: any) => {
               </label>
               <input
                 type="text"
-                id="name"
-                {...register("name", { required: "Nombre es requerido" })}
+                id="pet_name"
+                {...register("pet_name", { required: "Nombre es requerido" })}
                 className="w-full lg:mt-2 p-2 border border-gray-800 rounded-md"
               />
-              {errors.name && typeof errors.name.message === "string" && (
-                <p className="text-red-500">{errors.name.message}</p>
-              )}
+              {errors.pet_name &&
+                typeof errors.pet_name.message === "string" && (
+                  <p className="text-red-500">{errors.pet_name.message}</p>
+                )}
             </div>
             <div className="lg:mr-16 lg:mt-10 lg:mb-10">
               <label
@@ -357,7 +345,7 @@ const submit = async (data: any) => {
                 Edad:
               </label>
               <input
-                type="number"
+                type="text"
                 id="age"
                 {...register("age", {
                   pattern: {
@@ -382,16 +370,18 @@ const submit = async (data: any) => {
                 Descripción:
               </label>
               <textarea
-                id="description"
-                {...register("description", {
+                id="pet_description"
+                {...register("pet_description", {
                   required: "La descripción es obligatoria",
                 })}
                 placeholder="Escribe características de tu mascota como: color, apodo, collar, etc..."
                 className="w-full p-2 lg:mt-2 border border-gray-800 rounded-md"
               ></textarea>
-              {errors.description &&
-                typeof errors.description.message === "string" && (
-                  <p className="text-red-500">{errors.description.message}</p>
+              {errors.pet_description &&
+                typeof errors.pet_description.message === "string" && (
+                  <p className="text-red-500">
+                    {errors.pet_description.message}
+                  </p>
                 )}
             </div>
             <div className="lg:mr-4 lg:mt-10 lg:mb-10 mb-4">
@@ -418,26 +408,23 @@ const submit = async (data: any) => {
           </div>
           <div className="mb-4">
             <label
-              htmlFor="description"
+              htmlFor="title"
               className="lg:ml-0 mb-2 font-semibold"
               style={{ display: "block", textAlign: "center" }}
             >
-              Descripción de la publicación:
+              Titulo:
             </label>
             <textarea
-              id="descriptionPublication"
-              {...register("descriptionPublication", {
+              id="title"
+              {...register("title", {
                 required: "La descripción de la publicación es obligatoria",
               })}
               className="w-full p-2 lg:mt-2 border border-gray-800 rounded-md"
               style={{ display: "block", margin: "0 auto" }}
             ></textarea>
-            {errors.descriptionPublication &&
-              typeof errors.descriptionPublication.message === "string" && (
-                <p className="text-red-500">
-                  {errors.descriptionPublication.message}
-                </p>
-              )}
+            {errors.title && typeof errors.title.message === "string" && (
+              <p className="text-red-500">{errors.title.message}</p>
+            )}
           </div>
           <div className="flex lg:justify-end justify-center">
             <button
